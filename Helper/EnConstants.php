708681cfed63e2a758a6ca5bc914d69689e02959
taylan89 <taylan@eniture-dev.com>
2023-10-04 12:38:22 +0500

    Implements GTZ new API, adds compatibility with the latest version of Magento, code improvements and bug fixes-updated.

---
+++ Helper/EnConstants.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Eniture\GlobalTranzLTLFreightQuotes\Helper;
+
+class EnConstants
+{
+   const PLAN_URL      = 'https://ws053.eniture.com/web-hooks/subscription-plans/create-plugin-webhook.php';
+   const QUOTES_URL    = 'https://ws053.eniture.com/v3.0/index.php';
+   const GOOGLE_URL    = 'https://ws053.eniture.com/addon/google-location.php';
+   const INDEX         = 'https://ws053.eniture.com/index.php';
+   const TEST_CONN_URL_WWE = 'https://ws053.eniture.com/carriers/wwe-freight/speedfreightTest.php';
+
+   const EN_URL        = 'https://eniture.com/magento-2-globaltranz-ltl-freight-quotes/';
+   const APP_CODE      = 'ENGlobalTranzLTL';
+
+}
